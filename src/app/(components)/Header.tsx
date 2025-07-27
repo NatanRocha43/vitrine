0bc2610787c39802e13a2a171178e94b2993dc70
Natan Rocha <natan.paularocha@gmil.com>
2025-07-26 21:26:58 -0300

    (fix): Correction in the header menu

--- src/app/(components)/Header.tsx
+++ src/app/(components)/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
                     <h2 className="text-lg font-bold tracking-tight">Fashionista</h2>
                 </div>
 
-                <nav className="hidden md:flex items-center gap-9">
+                <nav className="hidden lg:flex items-center gap-9">
                     <Link href="/new-arrivals" className="text-sm text-[#141414] font-medium">Novidades</Link>
                     <Link href="/men" className="text-sm text-[#141414] font-medium">Masculino</Link>
                     <Link href="/women" className="text-sm text-[#141414] font-medium">Feminino</Link>
@@ -23,45 +23,67 @@ export default function Header() {
                     <Link href="/sale" className="text-sm text-[#141414] font-medium">Promoções</Link>
                 </nav>
 
-                <div className="hidden md:flex gap-2">
-                    <button className="flex items-center justify-center cursor-pointer rounded-lg h-10 bg-[#ededed] text-[#141414] px-2.5 gap-2 text-sm font-bold" aria-label="Minha Conta">
+                <div className="hidden lg:flex gap-2">
+                    <button
+                        className="flex items-center justify-center cursor-pointer rounded-lg h-10 bg-[#ededed] text-[#141414] px-2.5 gap-2 text-sm font-bold"
+                        aria-label="Minha Conta">
                         <User className="w-5 h-5" />
                     </button>
-                    <button className="flex items-center justify-center cursor-pointer rounded-lg h-10 bg-[#ededed] text-[#141414] px-2.5 gap-2 text-sm font-bold" aria-label="Favoritos">
+                    <button
+                        className="flex items-center justify-center cursor-pointer rounded-lg h-10 bg-[#ededed] text-[#141414] px-2.5 gap-2 text-sm font-bold"
+                        aria-label="Favoritos">
                         <Heart className="w-5 h-5" />
                     </button>
-                    <button className="flex items-center justify-center cursor-pointer rounded-lg h-10 bg-[#ededed] text-[#141414] px-2.5 gap-2 text-sm font-bold" aria-label="Meu Carrinho">
+                    <button
+                        className="flex items-center justify-center cursor-pointer rounded-lg h-10 bg-[#ededed] text-[#141414] px-2.5 gap-2 text-sm font-bold"
+                        aria-label="Meu Carrinho">
                         <ShoppingBag className="w-5 h-5" />
                     </button>
                 </div>
-
-                <button onClick={() => setMenuOpen(!menuOpen)} className="md:hidden">
+                <button onClick={() => setMenuOpen(!menuOpen)} className="lg:hidden">
                     {menuOpen ? <X className="w-6 h-6 text-[#141414]" /> : <Menu className="w-6 h-6 text-[#141414]" />}
                 </button>
             </div>
 
             <div
-                className={`md:hidden absolute top-full left-0 w-full bg-white shadow-md transition-all duration-300 ease-in-out overflow-hidden ${menuOpen ? 'max-h-[auto] opacity-100' : 'max-h-0 opacity-0'}`}>
-                <div className="flex flex-col gap-2 pt-4">
-                    <button className="flex items-center rounded-lg h-10 bg-[#ededed] text-[#141414] px-3 gap-2 text-sm font-bold" aria-label="Minha Conta">
+                className={`lg:hidden absolute top-full left-0 w-full bg-white shadow-md transition-all duration-300 ease-in-out overflow-hidden ${menuOpen ? 'max-h-[auto] opacity-100' : 'max-h-0 opacity-0'
+                    }`}>
+                <div className="flex flex-col gap-2 pt-4 px-6">
+                    <button
+                        className="flex items-center rounded-lg h-10 bg-[#ededed] text-[#141414] px-3 gap-2 text-sm font-bold"
+                        aria-label="Minha Conta">
                         <User className="w-5 h-5" />
                         Conta
                     </button>
-                    <button className="flex items-center rounded-lg h-10 bg-[#ededed] text-[#141414] px-3 gap-2 text-sm font-bold" aria-label="Favoritos">
+                    <button
+                        className="flex items-center rounded-lg h-10 bg-[#ededed] text-[#141414] px-3 gap-2 text-sm font-bold"
+                        aria-label="Favoritos">
                         <Heart className="w-5 h-5" />
                         Favoritos
                     </button>
-                    <button className="flex items-center rounded-lg h-10 bg-[#ededed] text-[#141414] px-3 gap-2 text-sm font-bold" aria-label="Meu Carrinho">
+                    <button
+                        className="flex items-center rounded-lg h-10 bg-[#ededed] text-[#141414] px-3 gap-2 text-sm font-bold"
+                        aria-label="Meu Carrinho">
                         <ShoppingBag className="w-5 h-5" />
                         Carrinho
                     </button>
                 </div>
                 <nav className="flex flex-col px-6 py-4 gap-4">
-                    <Link href="/new-arrivals" className="text-sm text-[#141414] font-medium">Novidades</Link>
-                    <Link href="/men" className="text-sm text-[#141414] font-medium">Masculino</Link>
-                    <Link href="/women" className="text-sm text-[#141414] font-medium">Feminino</Link>
-                    <Link href="/accessories" className="text-sm text-[#141414] font-medium">Acessórios</Link>
-                    <Link href="/sale" className="text-sm text-[#141414] font-medium">Promoções</Link>
+                    <Link href="/new-arrivals" className="text-sm text-[#141414] font-medium">
+                        Novidades
+                    </Link>
+                    <Link href="/men" className="text-sm text-[#141414] font-medium">
+                        Masculino
+                    </Link>
+                    <Link href="/women" className="text-sm text-[#141414] font-medium">
+                        Feminino
+                    </Link>
+                    <Link href="/accessories" className="text-sm text-[#141414] font-medium">
+                        Acessórios
+                    </Link>
+                    <Link href="/sale" className="text-sm text-[#141414] font-medium">
+                        Promoções
+                    </Link>
                 </nav>
             </div>
         </header>
